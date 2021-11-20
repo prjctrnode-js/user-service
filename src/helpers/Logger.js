5d5259afc5ce387310d5803afbf374ff42f70aa8
andrey Nemodruk <nemodruk.briz.ua>
2021-11-20 17:58:32 +0200

    fix addLogger

--- src/helpers/Logger.js
+++ src/helpers/Logger.js
@@ -1,14 +1,30 @@
 const winston = require('winston');
 const logConfiguration = {
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'MMM-DD-YYYY HH:mm:ss',
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'MMM-DD-YYYY HH:mm:ss',
+        }),
+        winston.format.printf(
+          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+        )
+      ),
     }),
-    winston.format.printf(
-      (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
-    )
-  ),
+    new winston.transports.Console({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({
+          format: 'MMM-DD-YYYY HH:mm:ss',
+        }),
+        winston.format.printf(
+          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+        )
+      ),
+    }),
+  ],
 };
 const logger = winston.createLogger(logConfiguration);
 
