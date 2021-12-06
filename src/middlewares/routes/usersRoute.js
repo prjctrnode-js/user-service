9e1b101f75e9e9bfdb090ac8568e1b9e1b9928f5
andrey Nemodruk <nemodruk.briz.ua>
2021-12-06 08:58:07 +0200

    fix routes

--- src/middlewares/routes/usersRoute.js
+++ src/middlewares/routes/usersRoute.js
@@ -1,18 +1,8 @@
 const Router = require('@koa/router');
 const axios = require('axios');
 const router = new Router();
-const pJson = require('../../../package.json');
 const db = require('../../db/models');
 const logger = require('../../helpers/logger');
-
-router.get('/users/health', async (ctx) => {
-  ctx.set({ 'Content-Type': 'application/json' });
-  ctx.status = 200;
-  ctx.body = JSON.stringify({
-    succes: true,
-    message: `Name ${pJson.name}, verion ${pJson.version}`,
-  });
-});
 
 router.post('/users', async (ctx) => {
   const { name, email } = ctx.request.body;
